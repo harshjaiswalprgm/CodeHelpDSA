18b0ccfef5aee31ac2ab77c58abf5a13b164c9b4
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-10-22 14:04:51 +0530

    update

---
+++ basicoperator.cpp
@@ -0,0 +1,17 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a = 5;
+//     int b =2;
+//     int c= b/a;
+
+//     cout<<c<<endl;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+    float b =2.0/5;
+
+    cout<<b<<endl;
+}
