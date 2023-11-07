8317577e42b397def230c318238b010a52441400
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-11-07 20:19:57 +0530

    update

--- whileloop3.cpp
+++ whileloop3.cpp
@@ -2,17 +2,15 @@
 using namespace std;
 int main()
 {
-    int a; // sum of all odd number**
+    int a; // print all odd numbers**
     cout << "enter the number :" << endl;
     cin >> a;
     int i = 1;
-    int sum = 0;
     while (i <= a)
     {
-
-        sum = sum + i;
+        cout << i << " ";
         i = i + 2;
     }
-    cout << "sum of odd no. is :" << sum << endl;
+
     return 0;
 }
