168fa2f1773c5b0b45449e3ed3cc12d8a0aa3b38
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2023-11-07 19:34:58 +0530

    update

--- typecasting.cpp
+++ typecasting.cpp
@@ -24,11 +24,3 @@
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
-int main()  //printing the value of ascii table **int to char**
-{
-    unsigned int bh1 = -125;  
-    cout << bh1 << endl;
-    return 0;
-}
---
+++ unsignednumber.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()  //printing the value of ascii table **int to char**
+{
+    unsigned int bh1 = -125;  
+    //unsigned int bh1 = 125;  
+    cout << bh1 << endl;
+    return 0;
+}
