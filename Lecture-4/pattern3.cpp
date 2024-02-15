078c232041731abedf3ea28c15a4c17e06c73fba
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-02-15 14:39:54 +0530

    update

--- Lecture-4/pattern3.cpp
+++ Lecture-4/pattern3.cpp
@@ -3,25 +3,25 @@ using namespace std;
 int main()
 {
 
-    // int n;
-    // cin >> n;
+    int n;
+    cin >> n;
 
-    // int i = 1;
-    // while (i <= n)
-    // {
-    //     int j = 1;
-    //     while (j <= n)
-    //     {
-    //         cout << j; // cout<<n-j+1; to print the reverce
-    //         j = j + 1;
-    //     }
-    //     cout << endl;
-    //     i = i + 1;
-    // }
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << j; // cout<<n-j+1; to print the reverce
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
 
-    int i = 6;
+    /*int i = 6;
     ///out << i++;
     cout<<++i;  //pre-increnment
-
+*/
     return 0;
 }
