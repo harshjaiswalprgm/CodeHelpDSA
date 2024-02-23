4788bbc9db281dd39430dd0ad10455b775120101
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-02-23 22:19:38 +0530

    update

--- Lecture-4/pattern1.cpp
+++ Lecture-4/pattern1.cpp
@@ -1,21 +1,44 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int i = 1;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         while (j <= n)
+//         {
+//             cout << " *";
+//             j = j + 1;
+//         }
+//         cout<<endl;
+//         i = i + 1;
+
+//     }
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 int main()
 {
     int n;
-    cin >> n;
+
+    cout << "enter the no. that u want to print the character" << endl;
     int i = 1;
+    cin >> n;
     while (i <= n)
     {
         int j = 1;
         while (j <= n)
         {
-            cout << " *"; 
+            cout << "*";
             j = j + 1;
         }
-        cout<<endl;
+        cout << endl;
         i = i + 1;
-        
     }
     return 0;
 }
