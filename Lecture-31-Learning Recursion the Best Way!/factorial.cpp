f26a3619ac369b7bb07da2caa3873c9e7c2a3dbb
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-03-23 22:04:46 +0530

    update

---
+++ Lecture-31-Learning Recursion the Best Way!/factorial.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    // base case
+
+    if (n == 0)
+        return 1;
+    // int chotti = factorial(n - 1); 
+    // int badi = n * chotti;
+
+    // int smallerPrblm = factorial(n - 1);
+    // int bigerPrblm = n * smallerPrblm;
+    // return bigerPrblm;
+
+    // int smallerPrblm = factorial(n - 1);
+    // return n * smallerPrblm;
+
+    return  n * factorial(n-1);
+}   
+int main()
+{
+    int n;
+    cin >> n;
+    int ans = factorial(n);
+    cout << ans << endl;
+    return 0;
+}
