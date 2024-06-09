8686142f04a9816278b1fb8ffa0c6adda1621ee9
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-06-10 00:21:54 +0530

    update

--- Week1/Loops/do-while.cpp
+++ Week1/Loops/do-while.cpp
@@ -3,5 +3,15 @@ using namespace std;
 int main()
 {
     int i = 1;
+    int count = 10;
+    // do{
+    //     cout << "Iteration : " << i << endl;
+    //     i++;
+    // } while (i <= 5);
+
+    do{
+        cout << "Harsh" << endl;
+        i++;
+    } while (i <= count);
     return 0;
 }
