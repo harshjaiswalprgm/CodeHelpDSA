5b580c3a7c671837e746d076d6c02c1215dc393c
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-06-09 08:31:36 +0530

    update

--- Week1/Loops/forwhile.cpp
+++ Week1/Loops/forwhile.cpp
@@ -2,16 +2,28 @@
 using namespace std;
 int main()
 {
-    // for (int i = 0; i < 10; i++){
+    // int i = 1;
+    // for (int i = 100; i>=1;  i--){  //print 100 to 1
     //     cout<<" "<<i<<endl;
     // }
 
-    int i = 0;
-    while (i <= 5)
-    {
-        cout << i<< " "; 
-        i = i+1;
+     int i = 0;
+    for (int i = 0; i>=-10;  i--){  //print 0 to -10
+        cout<<" "<<i<<endl;
     }
+
+
+    // while(i>= -10){
+    //     cout << i << " " << endl;
+    //     i--;
+    // }
+
+    // int i = 0;
+    // while (i <= 5)
+    // {
+    //     cout << i<< " "; 
+    //     i = i+1;
+    // }
     
 
     return 0;
