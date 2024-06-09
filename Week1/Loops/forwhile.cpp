2fded1c6b8ff26c777d73d6f7136954491759346
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-06-09 08:14:52 +0530

    update

---
+++ Week1/Loops/forwhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // for (int i = 0; i < 10; i++){
+    //     cout<<" "<<i<<endl;
+    // }
+
+    int i = 0;
+    while (i <= 5)
+    {
+        cout << i<< " "; 
+        i = i+1;
+    }
+    
+
+    return 0;
+}
