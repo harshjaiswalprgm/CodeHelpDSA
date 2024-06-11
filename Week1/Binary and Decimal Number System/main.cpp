c1650de81dd76938657aca3fd43a9f3867113657
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-06-11 21:17:57 +0530

    update

--- Week1/Binary and Decimal Number System/main.cpp
+++ Week1/Binary and Decimal Number System/main.cpp
@@ -1,19 +1,51 @@
+// #include <iostream>
+// using namespace std;
+
+// int decimalToBinaryMethod1(int n)
+// {
+//     // Division Method
+//     while (n > 0)
+//     {
+//         int bit = n % 2;
+//         cout << bit << endl;
+//         n = n / 2;
+//     }
+//     return 0;
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int binary = decimalToBinaryMethod1(n);
+//     return 0;
+// }
+
+// To finding individualy bits--->>>>>>>
+
 #include <iostream>
+#include<cmath>
 using namespace std;
 
-int decimalToBinaryMethod1(int n){
-    //Division Method
-    while(n>0){
+
+int decimalToBinaryMethod1(int n)
+{
+    // Division Method
+    int binaryno = 0;
+    int i = 0;
+    while (n > 0)
+    {
         int bit = n % 2;
-        cout << bit << endl;
+        binaryno = bit*pow(10, i++) + binaryno;  //formula to print int integer-->>
+
         n = n / 2;
     }
-    return 0;
+    return binaryno;
 }
 int main()
 {
     int n;
     cin >> n;
     int binary = decimalToBinaryMethod1(n);
+    cout << binary << endl;
     return 0;
 }
