cf780c08790783ed5ce4708d339c4c3e7653e0cf
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-06-23 18:10:55 +0530

    new update

---
+++ Week1/Functionfldr/prime.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+bool isPrime(int number)
+{
+    for (int i = 2; i <= (number - 1); i++)
+    {
+        if (number % 2 == 0)
+        {
+            return false;
+        }
+    }
+    // agar yhaa tak aagye to prime no. ho
+    return true;
+}
+int main()
+{
+    bool ans = isPrime(12);
+    if (ans == true)
+    {
+        cout << "NoisPrime" << endl;
+    }
+    else
+    {
+        cout << "NoisNotPrime" << endl;
+    }
+    return 0;
+}
