ddcd3877c80c6d287df8d65e73817c5bee8c60a8
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-08-01 21:12:46 +0530

    update

--- CodeHelpYoutube/Lecture-20-ArrayQuestion/reverse.cpp
+++ CodeHelpYoutube/Lecture-20-ArrayQuestion/reverse.cpp
@@ -1,32 +1,36 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-vector<int> reverse(vector<int> v) {
+vector<int> reverse(vector<int> v)
+{
 
-    int s = 0, e =v.size()-1;
+    int s = 0, e = v.size() - 1;
 
-    while(s<=e) {
+    while (s <= e)
+    {
         swap(v[s], v[e]);
         s++;
         e--;
     }
 
     return v;
-
 }
 
-void print(vector<int> v) {
+void print(vector<int> v)
+{
 
-    for(int i=0; i<v.size(); i++) {
+    for (int i = 0; i < v.size(); i++)
+    {
         cout << v[i] << " ";
     }
     cout << endl;
 }
 
-int main() {
+int main()
+{
 
-    vector<int> v ;
+    vector<int> v;
 
     v.push_back(11);
     v.push_back(7);
@@ -36,9 +40,8 @@ int main() {
 
     vector<int> ans = reverse(v);
 
-    cout << "Printing reverse Array" <<endl;
+    cout << "Printing reverse Array" << endl;
     print(ans);
-
 
     return 0;
 }
