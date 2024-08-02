47715daccd4c7c1a6d96ebe71d376e5c912cb870
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-08-02 23:11:45 +0530

    update

---
+++ Week2/SearchingTechniques/LinerSearch.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+bool targetFound(int arr[], int size, int target)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int currentElement = arr[i];
+        if (currentElement == target)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+int main()
+{
+    // int arr[] = {10, 20, 30, 40, 50};
+    // int size = 5;
+    // int target = 330;
+    // bool found = targetFound(arr, size, target);
+    // cout << found << " ";
+
+    // This is the basic way to find the element;
+
+    //Now let's take the input by the user end & then we will find the element!
+
+    int arr[100];
+    int size;
+    cout << "Enter the no. of elements in the Array" << endl;
+    cin >> size;
+
+    for (int i = 0; i < size; i++){
+        cout << "Enter the input value for the index" <<i <<endl;
+        cin >> arr[i];
+    }
+
+        return 0;
+}
