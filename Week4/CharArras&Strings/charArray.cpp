70828bad1cd8408a193456c3508d620f79b47148
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-01 12:45:55 +0530

    update

--- Week4/CharArras&Strings/charArray.cpp
+++ Week4/CharArras&Strings/charArray.cpp
@@ -6,7 +6,7 @@ int main()
     char arr[10];
     cout << "enter your name" << endl;
     cin >> arr;
-    cout << "your name is " << arr << endl;
+    cout << "Your name is " << arr << endl;
 
     return 0;
 }
