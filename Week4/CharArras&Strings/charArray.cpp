b9e541d988e12b5f3cc2e358e116c51d8ac27b30
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-01 13:07:38 +0530

    update

--- Week4/CharArras&Strings/charArray.cpp
+++ Week4/CharArras&Strings/charArray.cpp
@@ -8,5 +8,15 @@ int main()
     cin >> arr;
     cout << "Your name is " << arr << endl;
 
+
+    //To know that the null key is present or not we typecast the characters to 
+    //ASCII values let's seee
+
+    cout << arr[0] << "->" << (int)arr[0] << endl;
+    cout << arr[1] << "->" << (int)arr[1] << endl;
+    cout << arr[2] << "->" << (int)arr[2] << endl;
+    cout << arr[3] << "->" << (int)arr[3] << endl;
+    cout << arr[4] << "Here we print zero=>" << (int)arr[4] << endl;
+
     return 0;
 }
