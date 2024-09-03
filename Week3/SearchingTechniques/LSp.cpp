89b3cc4ebb219c955c0f60553b754db2db411e40
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-03 11:29:21 +0530

    update

--- Week3/SearchingTechniques/LSp.cpp
+++ Week3/SearchingTechniques/LSp.cpp
@@ -1,26 +1,42 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-bool findTarget(int arr[], int size, int target){
+bool findTarget(int arr[], int size, int target)
+{
 
-    //tarversing krenge
-    for(int i = 0; i < size; i++){
+    // tarversing krenge
+    for (int i = 0; i < size; i++)
+    {
 
-        if(arr[i] == target){
+        if (arr[i] == target)
+        {
             return true;
         }
-       
-
     }
     return false;
 }
 
 int main()
 {
-    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
-    int size = 8;
-    int target = 40;
+    // int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
+    // int size = 8;
+    // int target = 40;
+    // bool ans = findTarget(arr, size, target);
+    int arr[10];
+    int size;
+    cout<<"enter the size of an array"<<endl;
+    cin >> size;
+
+    for (int i = 0; i < size; i++){
+        cout << "enter the value of the index " << i << endl;
+        cin >> arr[i];
+
+    }
+    int target;
+    cout << "enter the target " << endl;
+    cin >> target;
+
     bool ans = findTarget(arr, size, target);
-    cout << "Target is : " << ans << endl;
+    cout<< "Target is : " << ans << endl;
     return 0;
 }
