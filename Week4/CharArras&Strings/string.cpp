03cf5b46174e3da67e956c8cb0542fbad2a9474c
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-08 17:57:37 +0530

    update

---
+++ Week4/CharArras&Strings/string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+string str;
+
+str.push_back('l');
+str.push_back('o');
+str.push_back('v');
+str.push_back('e');
+str.pop_back();
+// cout<<"enter the string"<<endl;
+// cin >> str;
+cout << "The string is :"<<str<<endl;
+// cout<<str[0]<<endl;
+    return 0;
+}
