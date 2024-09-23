8add805f5eda2f7acfa1246e7ff1bb44b4c087e7
Harsh Jaiswal <136101726+harshjaiswalprgm@users.noreply.github.com>
2024-09-23 22:31:49 +0530

    update

--- CodeHelpYoutube/Lecture-12-BinarySearch/binarySearch.cpp
+++ CodeHelpYoutube/Lecture-12-BinarySearch/binarySearch.cpp
@@ -34,7 +34,7 @@ int main() {
     int even[6] = {2,4,6,8,12,18};
     int odd[5] = {3, 8, 11, 14, 16};
 
-    int evenIndex = binarySearch(even, 6, 4);
+    int evenIndex = binarySearch(even, 6, 8);
 
     cout << " Index of 6 is " << evenIndex << endl;
 
